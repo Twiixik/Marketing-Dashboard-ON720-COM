4189c05d8c4a6f8d31254019bb67731aad6eb5ca
Twiixik <119367167+Twiixik@users.noreply.github.com>
2025-05-13 14:34:26 +0200

    Deployment Configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css"; 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/dashboard-on720.com/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Marketing-Dashboard-ON720-COM/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
